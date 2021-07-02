aa8bfa9428a3198f16101f1093deaa57339278bd
19wh1a1220 <81095065+mds56@users.noreply.github.com>
2021-07-02 15:52:02 +0530

    Delete JAVA LAB directory

--- JAVA LAB/Test1.java
+++
@@ -1,8 +0,0 @@
-package P1;
-class Test1
-{ 
-  public void display()
-   {
-    System.out.println("Hello p1");
-}
-}
--- JAVA LAB/multiplynumbers.java
+++
@@ -1,9 +0,0 @@
-class multiplynumbers
-{
-public static void main(String[] args)
-{
-int n1=5,n2=15, multiply;
-multiply = n1 * n2;
-System.out.println("The product is :"+multiply);
-}
-}
